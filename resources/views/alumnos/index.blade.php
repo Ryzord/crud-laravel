523ad10cbe537b0c0c2c5017bd43f873fe664b31
Ryzord <herrera_ruben1@hotmail.com>
2024-05-20 00:03:43 +0200

    Arreglado el problema que tenia al ejecutar laravel. Me daba el siguiente error \\\ SQLSTATE[42S22]: Column not found: 1054 Unknown column 'payload' in 'field list' ///. Ya está ejecutando bien la vista que tenia de "Listado de alumnos".
    Signed-off-by: Ryzord <herrera_ruben1@hotmail.com>

---
+++ resources/views/alumnos/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/template')
+
+@section('tittle', 'Alumnos | Escuela')
+
+@section('contenido')
+
+<main>
+    <div class="container py-4">
+        <h2>Listado de alumnos</h2>
+    </div>
+</main>
